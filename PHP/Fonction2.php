aaf426ec751752de80cd88faea8e8b909967ac82
Ulysse <151675412+PepitoBailao@users.noreply.github.com>
2024-06-03 21:42:50 +0200

    Add files via upload

--- PHP/Fonction2.php
+++ PHP/Fonction2.php
@@ -1,62 +1,17 @@
-<?php 
-
-    function tester_grille($une_grille){
-        for ($cptl = 0; $cptl < TAILLE * TAILLE; $cptl++){
-            for ($cptc = 0; $cptc < TAILLE * TAILLE; $cptc++){
-                $val = $une_grille[$cptl][$cptc];
-                $une_grille[$cptl][$cptc] = 0;
-                if(!est_valide($une_grille, $cptl, $cptc, $val)){
-                    return FALSE;
-                }
-                $une_grille[$cptl][$cptc] = $val;
-            }        
+<?php
+function affiche_grille_html($grille) {
+    echo "<form method='post' action='index.php'>";
+    echo "<table>";
+    foreach ($grille as $ligne) {
+        echo "<tr>";
+        foreach ($ligne as $cellule) {
+            echo "<td><input type='number' min='1' max='9' value='" . $cellule . "'></td>";
         }
-        return TRUE;
+        echo "</tr>";
     }
-
-    function transpose_tableau($un_tableau){
-        $une_grille = array();
-        $cpt = 0;
-        for ($cptl = 0; $cptl < TAILLE * TAILLE; $cptl++){
-            $une_grille[$cptl]=array();
-            for ($cptc = 0; $cptc < TAILLE * TAILLE; $cptc++){
-                $une_grille[$cptl][$cptc] = $un_tableau[$cpt];
-                $cpt++;
-            }
-        }   
-        return $une_grille;
-    }
-    
-    
-    function alea_vide(){
-        if (mt_rand(1,100) < PROBA * 100 ){
-            return true;
-        }else{
-            return false;
-            }
-    };
-
-    function affiche_grille_html($une_grille){
-        $max = TAILLE*TAILLE;
-        echo "<h1>SUDOKU DE LA MORT QUI TUE</h1>";
-        echo "<form action='index.php' method='post'>";
-        echo "<table>";
-        foreach ($une_grille as $une_ligne) {
-            echo "<tr>";
-            foreach ($une_ligne as $une_case) {
-                echo "<td>";
-                if(alea_vide()){
-                    echo "<input type='number' name='tabVal[]' min='1' max='$max' size ='3'value='' required>";
-                } else{
-                    echo "<input type='number' name='tabVal[]' min='1' max='$max' size ='3'value='$une_case' readonly>";
-                }
-                echo "</td>";
-            }
-            echo "</tr>";
-        }
-        echo "</table>";
-        echo "<br>";
-        echo "<input type='submit' value='Tester la validité'>";
-        echo "</form>";
-    };
+    echo "</table>";
+    echo "<button type='submit'>Tester la validité</button>";
+    echo "</form>";
+}
 ?>
+
--- index.php
+++ index.php
@@ -4,28 +4,13 @@
     <meta charset="UTF-8">
     <meta name="author" content="Ulysse RICHARD">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SUDOKU DE LA MORT QUI TUE</title>
+    <title>SUDOKU DE LA MORT</title>
     <link href="CSS/style.css" rel="stylesheet">
 </head>
 <body>
     <?php
     const TAILLE = 2;
-    const PROBA = 0.4;
-    require("PHP/Fonction1.php");
-    require("PHP/Fonction2.php");
-
-    if(!isset($_POST['tabVal'])){ //Début du jeu
-        $grille = remplir_grille_ite();
-        affiche_grille_html($grille);
-    } else{ //Retour formulaire
-        $grille = transpose_tableau($_POST['tabVal']);
-        affiche_grille($grille);
-        if (tester_grille($grille)){
-            echo "Bravo";
-        } else{
-            echo "Grille non valide";
-        }
-    }
+    require("PHP/Fonction1.php")
     ?>
 </body>
 </html>
